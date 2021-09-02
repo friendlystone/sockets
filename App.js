3c4a5beabaa4ec2c992c536364b8b0ea83375311
friendlystone <georgiana.bumbar@yahoo.com>
2021-09-02 17:39:44 +0300

    Things
    Does not want to change the name.

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View 
+} from 'react-native';
+window.navigator.userAgent = 'react-native';
+import io from 'socket.io-client/dist/socket.io';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    name: 'Bob'
+  }
+
+  constructor() {
+    super();
+    this.socket = io('localhost:3000', {jsonp: false});
+    this.socket.on('update', () => this.setState({ name: 'Vasile' }))
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> { this.state.name } </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }  
 }
 
 const styles = StyleSheet.create({
